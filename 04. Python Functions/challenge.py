5ea073b4d7018b7c355b176926d54e051c43247d
Khaled <codepower.help@gmail.com>
2025-01-07 16:56:39 +0100

    functions challenge

---
+++ 04. Python Functions/challenge.py
@@ -0,0 +1,13 @@
+msg = input("enter you name")
+
+if msg == "quit":
+    print("Hello")
+
+
+# Example
+def calculate_total_price(quantity: int, price_per_item: float):
+    return quantity * price_per_item
+
+
+def get_user_name() -> str:
+    return "JohnDoe"
