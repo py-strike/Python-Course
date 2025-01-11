4648c2acb4fca631b19035d8d6839f0a62834d8f
Khaled <codepower.help@gmail.com>
2025-01-11 08:58:33 +0100

    new dictionary operations

--- 11. Dictionary Operations/main.py
+++ 11. Dictionary Operations/main.py
@@ -14,3 +14,41 @@ print(x, y, z)
 
 items = data.items()
 print(items)
+
+# -------------
+person = {
+    'name': 'khaled',
+    'age' : 18,
+    'hair_color' :'black'
+}
+
+changes = {
+    'name': 'k1'
+}
+
+additional_infos = {
+    'height': 6,
+    'drink_coffee' : False
+}
+
+print(person)
+person.update(changes)
+print(person)
+
+person.update(additional_infos)
+print(person)
+
+# -------------- coply dict
+d1 = person.copy()
+print(d1)
+
+# ---------- default
+person_2 = {
+    'name': 'kevin',
+    'age' : 18,
+    'hair_color' :'black'
+}
+
+person_2.setdefault('height', 'N/A')
+print(person_2)
+
