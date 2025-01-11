7f15bca295b84e7080c37ce63f7ca9d6070c4bc6
Khaled <codepower.help@gmail.com>
2025-01-11 06:20:35 +0100

    dictionary operations

---
+++ 11. Dictionary Operations/main.py
@@ -0,0 +1,16 @@
+# dict operations
+data = {'a':1, 'b':2, 'c':3}
+
+for k in data.keys():
+    print(k)
+
+for k in data.values():
+    print(k)
+
+t = (4, 5, 6)
+x, y, z = t
+
+print(x, y, z)
+
+items = data.items()
+print(items)
