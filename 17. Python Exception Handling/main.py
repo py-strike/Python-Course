ff99c6cf31265f015f99ef5eeb32dafaa4cb24df
Khaled <codepower.help@gmail.com>
2025-01-19 18:53:44 +0100

    Python Exception Handling

---
+++ 17. Python Exception Handling/main.py
@@ -0,0 +1,15 @@
+u = input("Enter a number : ")
+
+try:
+    number = int(u)
+    print(f"the num you entred plus one is : {number + 1}")
+except ValueError:
+    print("Is not a number")
+
+u2 = input("Enter a number : ")
+
+try:
+    number = int(u2)
+    print(f"the num you entred minus one is : {number - 1}")
+except ValueError:
+    print("This is not a number")
