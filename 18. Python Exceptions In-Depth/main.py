0de3e1df0f5eb81448b1410fc637c9a1baf368bc
Khaled <codepower.help@gmail.com>
2025-01-27 14:42:42 +0100

    Raising errors

--- 18. Python Exceptions In-Depth/main.py
+++ 18. Python Exceptions In-Depth/main.py
@@ -7,3 +7,18 @@
 # Index Error
 # Key Error
 # Zero Division Error
+
+# Raising errors
+def repeat_striung(s, nb_of_rep):
+    if type(s) is not str:
+        raise TypeError('The text must be a strings')
+    
+    if type(nb_of_rep) is not int:
+        raise TypeError('The nb of rep must be an integer')
+
+    return s*nb_of_rep
+
+s = input("Enter a text to be repeted : ")
+nb_of_rep = int(input("Enter the number od repetetion : "))
+
+print(repeat_striung(s, nb_of_rep))
