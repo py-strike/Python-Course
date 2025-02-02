82ebbf472f0ab03ff7176a381ac6fc38f281587c
Khaled <codepower.help@gmail.com>
2025-02-02 08:08:23 +0100

    working with boolean

---
+++ 19. Booleans/main.py
@@ -0,0 +1,13 @@
+x , y, z = 10, 7, 10
+
+if x is z :
+    print("x is z")
+else:
+    print("x is not z")
+
+a, b, c = {'a': 1, 'b': 2}, 5, {'a': 1, 'b': 2}
+
+if a == c :
+    print("a is c")
+else:
+    print("a is not c")
