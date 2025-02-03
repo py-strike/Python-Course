f4aa8f1dd966ae06dd673b6afde21903fdfbcbc8
Khaled <codepower.help@gmail.com>
2025-02-03 08:04:12 +0100

    Bubble Sort

--- 22. Basic Sorting Algorithm/main.py
+++ 22. Basic Sorting Algorithm/main.py
@@ -12,6 +12,22 @@ def selection_sort(nb):
 
 print(selection_sort([4, 101, -40 ,44, 8, 9, 9, -41, 12]))
 
+# Bubble Sort
+
+my_list_to_sort = [4, 101, -40 ,44, 8, 9, 9, -41, 12]
+
+def bubble_sort(my_list):
+    for _ in my_list:
+        for i in range(len(my_list) - 1):
+            if my_list[i] > my_list[i + 1]:
+                a = my_list[i + 1]
+                my_list[i + 1] = my_list[i]
+                my_list[i] = a
+                
+    return my_list
+
+print(bubble_sort(my_list_to_sort))
+
 
 
 
